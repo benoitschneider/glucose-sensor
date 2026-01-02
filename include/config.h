43f5918981317ca6a7c76337098ccf4ffaea6ee7
Benoît <benoit.schneider34@gmail.com>
2026-01-02 20:21:29 +0200

    feat(firmware): Update unified firmware tree

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Global configuration settings can be defined here
+#define FIRMWARE_VERSION_MAJOR 0
+#define FIRMWARE_VERSION_MINOR 1
+#define FIRMWARE_VERSION_PATCH 0
+
+#endif // CONFIG_H
